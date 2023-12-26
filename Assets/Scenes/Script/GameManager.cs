e28c8d611967a500706610577d0bb14558d349f7
moonseonu <48229760+moonseonu@users.noreply.github.com>
2023-12-26 23:17:54 +0900

    12/26

---
+++ Assets/Scenes/Script/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance != null) Destroy(gameObject);
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
